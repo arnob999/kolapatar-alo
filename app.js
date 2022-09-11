64e78fbda3a4e643f0890c45092de4cdd9b64a02
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2022-09-12 02:57:56 +0600

    Modal Foot Added

--- app.js
+++ app.js
@@ -49,6 +49,21 @@ const loadNews = async (id) => {
 };
 
 const showNews = (newses) => {
+
+
+
+    const counter = document.getElementById("counter");
+    counter.innerHTML = `
+
+    <div class="pt-2 pb-1" >
+    <p class="text-center">
+        There Are ${newses.length} item found of this category
+    </p>
+</div>
+    
+
+    `
+
     const newsSection = document.getElementById("mainNews");
 
     let sortedItems = [...newses];
@@ -58,6 +73,8 @@ const showNews = (newses) => {
     });
     newsSection.innerText = ``;
     for (const news of sortedItems) {
+
+
         const makeDiv = document.createElement("div");
         const thumbPic = news.thumbnail_url;
         // console.log(news._id);
@@ -90,16 +107,12 @@ const showNews = (newses) => {
                           <!-- author image -->
   
                           <span> 
-                          <img class=" authorImage rounded-circle " src=" ${news.author.img
-            } " alt="">
+                          <img class=" authorImage rounded-circle " src=" ${news.author.img} "
                           </span>
   
   
                           <span>
-                          ${news.author.name
-                ? news.author.name
-                : "No Aurhor Found"
-            }
+                          ${news.author.name ? news.author.name : "No Aurhor Found"}
                           </span>
   
                       </div>
@@ -118,7 +131,7 @@ const showNews = (newses) => {
                       <!-- Button trigger modal -->
   <button type="button" onclick= "loadModal('${news._id
             }')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
+    See Details <i class=" ps-2 bi bi-arrow-right-circle-fill"></i>
   </button>
   
   <!-- Modal -->
@@ -131,18 +144,65 @@ const showNews = (newses) => {
     toggler(false);
 };
 const loadModal = (newsID) => {
-    console.log(newsID);
+
     fetch(`https://openapi.programming-hero.com/api/news/${newsID}`)
         .then((res) => res.json())
         .then((data) => displayModal(data.data[0]));
 };
 const displayModal = (data) => {
+
     console.log(data);
+
 
     const title = document.getElementById("exampleModalLabel");
     title.innerHTML = `${data.title}`;
+
+
     const modalBody = document.getElementById("body");
-    modalBody.innerHTML = `    <img class="modal-img w-100" src=" ${data.image_url} " alt="">
+    modalBody.innerHTML = ` <img class=" w-100 modal-img" src=" ${data.image_url} " alt="">
     <p> ${data.details}  </p>
+    <br>
+    <br>
+
+    <div class=" d-flex justify-content-between ">
+
+
+    <div class="d-flex ">
+
+        <div>
+            <img class="authorImage" src="${data.author.img}" alt="">
+        </div>
+        <div class="ps-1">
+            <div>
+                <!-- author name -->
+                ${data.author.name}
+            </div>
+            <div>
+                <!-- date -->
+                ${data.author.published_date.slice(0, 11) ? data.author.published_date.slice(0, 11) : "No Author Found"}
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        <!-- view COunt -->
+        <i class="bi bi-eye-fill"></i>
+       ${data.total_view ? data.total_view : "No View"}
+    </div>
+
+
+    <div>
+        <!-- Rating -->
+        <i class="bi bi-star-fill"></i>
+        ${data.rating.number ? data.rating.number : "No Rating"}
+    </div>
+
+
+
+</div>
+    
     `;
+
+
 };
